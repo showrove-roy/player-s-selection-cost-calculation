736c0ebb21b1268f49646c4d6039087b3c6c7525
Showrove Roy <showrove200@gmail.com>
2022-08-23 23:26:10 +0600

    player cost calculation done

--- js/app.js
+++ js/app.js
@@ -14,28 +14,54 @@ function setValue(iD, valuE) {
 }
 
 
+function getSetBtnValue(name, iD) {
+    const element = document.getElementById(iD);
+    element.setAttribute("disabled", true);
+    element.classList.remove('bg-[#015196]');
+    element.classList.add('bg-[#A2A9AF]');
+
+    const playerList = document.getElementById('player-list');
+    let playerName = document.createElement("li");
+    playerName.innerText = name;
+
+    playerList.appendChild(playerName);
+
+    let playerListNumber = playerList.childElementCount;
+
+    if (playerListNumber === 5) {
+        let allSelectBtns = document.getElementsByClassName('all-select-btn');
+        for (allSelectBtn of allSelectBtns) {
+            allSelectBtn.setAttribute("disabled", true);
+        }
+    };
+
+}
+
+
+
+
 document.getElementById('player-cost-btn').addEventListener('click', function () {
     const playerExpenses = getInputValue('per-player-cost');
-    setValue('player-expenses', playerExpenses);
+    const playerList = document.getElementById('player-list');
+    let playerListNumber = playerList.childElementCount;
+    let totalPlayerExpenses = playerExpenses * playerListNumber;
+    setValue('player-expenses', totalPlayerExpenses);
 });
 
 document.getElementById('total-cost-btn').addEventListener('click', function () {
-    const playerExpenses = getInputValue('per-player-cost');
+
 
     const managerFee = getInputValue('manager-fee');
     const coachFee = getInputValue('coach-fee');
 
-    const getElement = document.getElementById('player-expenses').innerText;
-    const playerexpenses = parseFloat(getElement);
-
-    const totalCost = playerexpenses + managerFee + coachFee;
+    const totalCost = totalPlayerExpenses + managerFee + coachFee;
 
     if (playerExpenses == 'NaN' || totalCost == "NaN") {
         alert('Please Enter Valid Input');
         return;
 
     } else {
-        setValue('player-expenses', playerExpenses);
+        setValue('player-expenses', totalPlayerExpenses);
         setValue('total-cost', totalCost);
         return;
     };
